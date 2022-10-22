33cfe73dee9fc675c24a9969c8e6cb288bae39b8
gfCrova <giancrova.cl5@gmail.com>
2022-10-22 00:58:39 -0300

    se agrego un icono y modifico el css

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@
 const cambiarModo = () => {
     let cuerpoweb = document.body;
     cuerpoweb.classList.toggle('oscuro');
-
-    let cardEstudiante = document.body.querySelector('.est')
-    cardEstudiante.classList.toggle('cardEst');
-
-    let cardTrainee = document.body.querySelector('.tra')
-    cardTrainee.classList.toggle('cardTra');
-
-    let cardJunior = document.body.querySelector('.jr')
-    cardJunior.classList.toggle('cardJr');
 }
 
 // TEXTO VALOR DE TICKET
@@ -32,21 +23,22 @@ const resumenFunc = () => {
 
     let cantidad = document.getElementById('cantidad').value;
     let categoria = document.getElementById('categoria').value;
+    let mostrarResultado = document.getElementById('txtPagar');
     let ticket = 200;
     let total;
 
     if (categoria == 'Estudiante') {
         total = (ticket - (ticket * 80) / 100) * cantidad;
-        document.getElementById('txtPagar').innerHTML = `Total a pagar: \u0024 ${total}`;
+        mostrarResultado.innerHTML = `Total a pagar: $ ${total}`;
     } else if (categoria == 'Trainee') {
         total = (ticket - (ticket * 50) / 100) * cantidad;
-        document.getElementById('txtPagar').innerHTML = `Total a pagar: \u0024 ${total}`;
+        mostrarResultado.innerHTML = `Total a pagar: $ ${total}`;
     } else if (categoria == 'Junior') {
         total = (ticket - (ticket * 15) / 100) * cantidad;
-        document.getElementById('txtPagar').innerHTML = `Total a pagar: \u0024 ${total}`;
+        mostrarResultado.innerHTML = `Total a pagar: $ ${total}`;
     } else if (categoria == 'Ninguna') {
         total = ticket * cantidad;
-        document.getElementById('txtPagar').innerHTML = `Total a pagar: \u0024 ${total}`;
+        mostrarResultado.innerHTML = `Total a pagar: $ ${total}`;
     }
 };
 
@@ -54,7 +46,7 @@ const borrarFunc = () => {
     document.getElementById('name').value = ' ';
     document.getElementById('apellido').value = ' ';
     document.getElementById('email').value = ' ';
-    document.getElementById('cantidad').value = ' ';
-    document.getElementById('categoria').value = 'Ninguna';
-    document.getElementById('txtPagar').innerHTML = 'Total a pagar: \u0024';
+    cantidad.value = ' ';
+    categoria.value = 'Ninguna';
+    document.getElementById('txtPagar').innerHTML = 'Total a pagar: $';
 };
