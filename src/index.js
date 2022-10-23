9179ad2d31fce3189d37c37804839233de9092c9
gfCrova <giancrova.cl5@gmail.com>
2022-10-23 15:54:55 -0300

    ultimos detalles

--- src/index.js
+++ src/index.js
@@ -22,13 +22,14 @@ new TypeIt("#textTicket", {
 const resumenFunc = () => {
 
     let cantidad = document.getElementById('cantidad').value;
+    cantidad = parseInt(cantidad);
     let categoria = document.getElementById('categoria').value;
     let mostrarResultado = document.getElementById('txtPagar');
     let error = document.getElementById('error');
     let ticket = 200;
     let total;
 
-    if (cantidad > 0) {
+    if (cantidad > 0 || !cantidad == Number) {
         if (categoria == 'Estudiante') {
             total = (ticket - (ticket * 80) / 100) * cantidad;
             mostrarResultado.innerHTML = `Total a pagar: $ ${total}`;
@@ -54,4 +55,5 @@ const borrarFunc = () => {
     cantidad.value = ' ';
     categoria.value = 'Ninguna';
     document.getElementById('txtPagar').innerHTML = 'Total a pagar: $';
+    error.classList.remove('visible');
 };
